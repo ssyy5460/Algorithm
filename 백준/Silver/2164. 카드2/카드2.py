a8e0feb2f51e3b4990ae0cfb3fc304a153005a82
Im_ss0 <64217874+ssyy5460@users.noreply.github.com>
2022-07-05 00:04:14 +0900

    [Silver IV] Title: 카드2, Time: 180 ms, Memory: 134364 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,9 @@
+from collections import deque
+N = int(input())
+deque = deque([i for i in range(1,N+1)])
+
+while (len(deque) > 1):
+    deque.popleft()
+    move_num = deque.popleft()
+    deque.append(move_num)
+print(deque[0])
