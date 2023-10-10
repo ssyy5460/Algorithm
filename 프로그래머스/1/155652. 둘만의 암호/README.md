c82b7c4ceb12a5b036c660f462df1d2ba7279565
Im_ss0 <64217874+ssyy5460@users.noreply.github.com>
2023-10-10 20:10:34 +0900

    [level 1] Title: 둘만의 암호, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/README.md
+++ 프로그래머스/1/155652. 둘만의 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 둘만의 암호 - 155652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.03 ms
+메모리: 10.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 20:0:9
+2023년 10월 2일 20:10:30
 
 ### 문제 설명
 
--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -4,8 +4,7 @@ def solution(s, skip, index):
     valid_alpha = [chr(x) for x in range(97, 123) if chr(x) not in skip]
 
     for v in s:
-        if v in valid_alpha:
-            idx = (valid_alpha.index(v) + index) % len(valid_alpha)
-            answer += valid_alpha[idx]
+        idx = (valid_alpha.index(v) + index) % len(valid_alpha)
+        answer += valid_alpha[idx]
     
     return answer
