fb36be73e13b94ab51fb25f7ee9c42614d70a28b
Im_ss0 <64217874+ssyy5460@users.noreply.github.com>
2024-03-21 20:22:44 +0900

    [Silver IV] Title: 동전 0, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,16 +1,22 @@
-n,k = map(int,input().split()) # 동전의 종류와 동전의 합
+# 동전의 종류와 구하고자 하는 동전의 합
+n,k = map(int,input().split())
+
+# 동전 리스트 
 coins = []
+
 for i in range(n):
-    coins.append(int(input())) # 동전들의 모임
-coins.sort(reverse = True) # 가장 큰 동전이 오게 역순으로 정렬
+    coins.append(int(input()))
+    
+# 역순으로 정렬
+coins.sort(reverse = True)
 
 cnt = 0
-for val in coins: 
-    if k == 0: # 동전의 합이 0이라면..
-        break
-    else:
-        while val <= k: # 동전의 합이 val보다 크거나 같다면
-            k -= val 
-            cnt += 1
-print(cnt)
-            
+
+if k == 0:
+    print(cnt)
+else:
+    for c in coins:
+        cnt += k // c
+        k %= c
+    print(cnt)
+        
